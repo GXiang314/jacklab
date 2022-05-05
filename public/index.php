ef76f9fa0dd15c572ac67fe0a17b6c43e716644b
GXiang314 <akz232032@gmail.com>
2022-05-05 23:34:06 +0800

    add route.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ namespace app\publics;
 
 use app\core\Application;
 use app\controllers\api\MemberController;
+use app\controllers\api\UserController;
+
 // use app\core\Application;
 
 require_once (__DIR__."../../vendor/autoload.php");
@@ -11,7 +13,8 @@ require_once (__DIR__."../../vendor/autoload.php");
 
 $app = new Application();
 
-$app->router->get('/api/manager/user',[new MemberController(),'index']);
+$app->router->get('/api/manager/user',[MemberController::class,'index']);
+$app->router->post('/api/manager/useradd',[UserController::class,'useradd']);
 
 $app->router->get('/user',[]);
 
