35f57a8cb54f74c973faba76e36975c2c1f58661
GXiang314 <akz232032@gmail.com>
2022-05-05 23:32:23 +0800

    user controller

--- app/controllers/api/UserController.php
+++ app/controllers/api/UserController.php
@@ -2,7 +2,9 @@
 namespace app\controllers\api;
 
 use app\controllers\BaseController;
+use app\core\Request;
 use app\services\MemberService;
+use Exception;
 
 class UserController extends BaseController{
 
@@ -12,4 +14,92 @@ class UserController extends BaseController{
         $this->memberService = new MemberService();
         
     }
+
+
+    public function index()
+    {
+        $data = $this->memberService->getAllMember();
+        return ($data !=[])? $this->sendResponse($data,'所有成員'):$this->sendError('沒有資料');
+    }
+
+    public function useradd(Request $request)
+    {
+        try {
+            $result = $request->getBody();
+            // $request['token'] = $this->memberService->generateAuthToken();
+            // $request['password'] = $this->memberService->generatePassword();
+            // $result = $this->memberService->studentAdd($request);
+            // $url = action([MemberController::class,'emailvalidate'],['account' => $request['account'],'token' => $request['token']]);
+            // $content = "使用者「{$request['name']}」，您好：\r\n\r\n您的帳號是：{$request['account']}\r\n您的密碼是：{$request['password']}\r\n\r\n請點擊以下連結以完成驗證步驟：\r\n{$url}";
+            // Mail::raw($content, function ($message) use ($request) {
+            //     $message
+            //     ->to($request['account'])
+            //     ->subject("創建帳號通知");
+            // });
+        } catch (Exception $e) {
+            return $this->sendError($e->getMessage(), 'Registered failed.', 500);
+        }
+        return $this->sendResponse($result,'success');
+    }
+/*
+    public function teacheradd(Request $request)
+    {
+        try {
+            // $request->validate([ //這邊會驗證註冊的資料是否符合格式
+            //     'account' => ['required', 'string', 'email', 'max:100','unique:member,Account'],
+            //     'password' => ['required'],
+            //     'name' => ['required', 'max:20'],
+            //     'title' => ['required', 'max:20'],
+            //     'role_Id' => ['required']
+            // ]);
+            $request['token'] = $this->memberService->generateAuthToken();
+            $result = $this->memberService->addTeacher($request);
+            $url = action([MemberController::class,'emailvalidate'],['account' => $request['account'],'token' => $request['token']]);
+            $content = "使用者「{$request['name']}」，您好：\r\n\r\n您的帳號是：{$request['account']}\r\n您的密碼是：{$request['password']}\r\n\r\n請點擊以下連結以完成驗證步驟：\r\n{$url}";
+            Mail::raw($content, function ($message) use ($request) {
+                $message
+                ->to($request['account'])
+                ->subject("創建帳號通知");
+            });
+        } catch (Exception $e) {
+            return $this->sendError($e->getMessage(), 'Registered failed.');
+        }
+        return $this->sendResponse($result,'success');
+    }
+
+    public function changeUserPassword(Request $request)
+    {
+        try {
+
+            $result = $this->memberService->updateUserPassword($request['account'],$request['password']);
+        } catch (Exception $e) {
+            return $this->sendError($e->getMessage(), 'Updating failed.');
+        }
+        if($result=='success'){
+            return $this->sendResponse($result,'success');
+        }
+        return $this->sendError($result);
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($student_id)
+    {
+        $result = $this->memberService->delete($student_id);
+        if($result == 'success'){
+            return $this->sendResponse($result,'刪除成功');
+        }
+        return $this->sendError($result,'刪除失敗，請稍後再試');
+    }*/
 }
