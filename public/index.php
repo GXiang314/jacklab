e4c2f8f01b77b0ac3589d72bfe1ebd307d0631bf
GXiang314 <akz232032@gmail.com>
2022-05-06 23:03:13 +0800

    add env & db config.

--- public/index.php
+++ public/index.php
@@ -1,17 +1,29 @@
 <?php
-namespace app\publics;
+namespace app\public;
 
 use app\core\Application;
 use app\controllers\api\MemberController;
 use app\controllers\api\UserController;
+use Dotenv\Dotenv;
 
 // use app\core\Application;
 
 require_once (__DIR__."../../vendor/autoload.php");
 
 
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
-$app = new Application();
+
+$config=[
+    'db'=>[
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application($config);
 
 $app->router->get('/api/manager/user',[MemberController::class,'index']);
 $app->router->post('/api/manager/useradd',[UserController::class,'useradd']);
@@ -19,5 +31,7 @@ $app->router->post('/api/manager/useradd',[UserController::class,'useradd']);
 $app->router->get('/user',[]);
 
 $app->router->post('/user',[]);
+
+// $app->db->applyMigrations();
 
 $app->run();
