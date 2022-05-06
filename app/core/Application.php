deb09030436b7eba04cf89b5035066f5b85171f1
GXiang314 <akz232032@gmail.com>
2022-05-06 22:59:26 +0800

    add db application.

--- app/core/Application.php
+++ app/core/Application.php
@@ -7,15 +7,18 @@ use ReflectionClass;
 
 class Application{
     public static Application $app;
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
     public Response $response;
-    public function __construct(){
+    public Database $db;
+    public function __construct(array $config){
         self::$app = $this;
+        self::$ROOT_DIR = dirname(__DIR__);
         $this->response = new Response();
         $this->request = new Request();
         $this->router = new Router($this->request,$this->response);
-
+        $this->db = new Database($config['db']);
     }
     public function run()
     {
