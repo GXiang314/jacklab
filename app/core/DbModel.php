61cb5550e17e67de76d1d81b63e3d531f1965f87
GXiang314 <akz232032@gmail.com>
2022-05-07 15:28:45 +0800

    set try catch with save return.

--- app/core/DbModel.php
+++ app/core/DbModel.php
@@ -2,6 +2,8 @@
 
 namespace app\core;
 
+use Exception;
+
 abstract class DbModel extends Model{
 
     abstract public function table(): string;
@@ -10,22 +12,26 @@ abstract class DbModel extends Model{
     
     public function save()
     {
-        $tablename = $this->table();
-        $attributes = $this->attributes();
-        $params = array_map(fn($attr)=>":$attr", $attributes);
-        $statement = self::prepare("
-        insert into $tablename(" . implode(',',$attributes) . ")        
-        values(" . implode(',',$params). "
-        );");
-        foreach($attributes as $attr){
-            $statement->bindValue(":$attr",$this->{$attr});
-        }
-        var_dump($statement->execute());
-
-
+        try{
+            $tablename = $this->table();
+            $attributes = $this->attributes();
+            $params = array_map(fn($attr)=>":$attr", $attributes);
+            $statement = self::prepare("
+            insert into $tablename(" . implode(',',$attributes) . ")        
+            values(" . implode(',',$params). "
+            );");
+            foreach($attributes as $attr){
+                $statement->bindValue(":$attr",$this->{$attr});
+            }
+            $statement->execute();
+        }catch(Exception){
+            return false;
+        }        
+        return true;
     }
 
     public static function prepare($sql){
         return Application::$app->db->pdo->prepare($sql);
     }
+    
 }
