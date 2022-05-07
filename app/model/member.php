bd34c82e243b1c1657caf160e0f140a76f17645c
GXiang314 <akz232032@gmail.com>
2022-05-07 15:30:27 +0800

    save function override.

--- app/model/member.php
+++ app/model/member.php
@@ -3,7 +3,7 @@
 namespace app\model;
 
 use app\core\DbModel;
-use DateTime;
+use app\services\MemberService;
 
 class member extends DbModel{
 
@@ -23,10 +23,23 @@ class member extends DbModel{
         return ['Account', 'Password', 'AuthToken', 'CreateTime', 'IsAdmin'];
     }
 
+    public function save()
+    {
+        $this->Password = MemberService::hash($this->Password);
+        $this->AuthToken = MemberService::generateAuthToken();
+        $this->CreateTime = date('Y-m-d h:i:s');
+        $this->IsAdmin = 0;
+        return parent::save();
+    }
+
     public function rules(): array
     {            
         return [
-            'Account' => [self::RULE_REQUIRED,[self::RULE_MAX,'max'=>50]],
+            'Account' => [
+                self::RULE_REQUIRED,
+                [self::RULE_MAX,'max'=>50],
+                [self::RULE_UNIQUE,'class'=>self::class]
+            ],
             'Password' => [self::RULE_REQUIRED],
 
         ];
