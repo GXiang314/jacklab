c3b4e6eb7825d2e79c73fe63db156f9f1873c479
GXiang314 <akz232032@gmail.com>
2022-05-07 15:29:05 +0800

    add function with json.

--- app/core/Response.php
+++ app/core/Response.php
@@ -9,5 +9,12 @@ class Response{
         return http_response_code($code);
     }
 
+    public static function json(array $params, $status = 200)
+    {
+        header('Content-type: application/json');
+        self::setStatusCode($status);
+
+        return json_encode($params);
+    }
 
 }
