a898f9225131d70e8705b020d1010e53e2a658e0
GXiang314 <akz232032@gmail.com>
2022-05-08 22:41:42 +0800

    update rules

--- app/requestModel/Useradd.php
+++ app/requestModel/Useradd.php
@@ -4,6 +4,7 @@
 namespace app\requestModel;
 
 use app\core\Model;
+use app\model\member;
 
 class Useradd extends Model{
 
@@ -18,7 +19,12 @@ class Useradd extends Model{
     public function rules(): array
     {
         return [
-            'Account' => [self::RULE_REQUIRED,self::RULE_EMAIL,[self::RULE_MAX,'max'=>50]],
+            'Account' => [
+                self::RULE_REQUIRED,
+                self::RULE_EMAIL,
+                [self::RULE_MAX,'max'=>50],
+                [self::RULE_UNIQUE,'class'=>member::class]
+            ],
             'Name' => [self::RULE_REQUIRED],
             'Class_Id' => [self::RULE_REQUIRED],
             'Role_Id' => [self::RULE_REQUIRED]
