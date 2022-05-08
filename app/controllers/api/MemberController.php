35a4c514eb1ebc0d7e6129916efc0a013254abe2
GXiang314 <akz232032@gmail.com>
2022-05-09 01:08:41 +0800

    add reset password actions.

--- app/controllers/api/MemberController.php
+++ app/controllers/api/MemberController.php
@@ -2,16 +2,20 @@
 namespace app\controllers\api;
 
 use app\core\Controller;
+use app\core\DbModel;
 use app\core\Request;
 use app\requestModel\ChangePassword;
+use app\requestModel\ResetPassword;
+use app\services\MailService;
 use app\services\MemberService;
 
 class MemberController extends Controller{
     private $memberService;
+    private $mailService;
     public function __construct()
     {
         $this->memberService = new MemberService();
-        
+        $this->mailService = new MailService();
     }
     /**
      * Get all resource in storage.
@@ -88,9 +92,96 @@ class MemberController extends Controller{
         }
         return $result=='success' ? $this->sendResponse($result,'success') : $this->sendError($result);
     }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \app\core\Request
+     * @return \app\core\Response
+     */
+    public function forgetPassword(Request $request)
+    {
+        if($request->isPost()){
+            $account = $request->getJson()['Account'] ?? '';
+            $data = $this->memberService->getAccountData($account);
+            if(!empty($data)){
+                $code = $this->memberService->generateAuthToken(6);
+                $this->mailService->sendForgetPasswordMail($data['Name'], $data['Account'], $code);
+                DbModel::create('reset_password',[
+                    'Account' => $data['Account'],
+                    'Update_at' => date('Y-m-d h:i:s',time() + 1800),
+                    'Code' => $code
+                ]);
+                
+                return $this->sendResponse("success","已發送驗證碼至您的信箱，請查收");
+            }
+            return $this->sendError('無此會員帳號');
+        }
+        return $this->sendError('Method Not Allow', [], 405);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \app\core\Request
+     * @return \app\core\Response
+     */
+    public function resetCodeValidate(Request $request)
+    {
+        if($request->isPost()){
+            $data = $request->getJson()?? '';
+            if(!empty($data)){
+                $checkdata = DbModel::findOne('reset_password', [
+                    'Account' => $data['Account'],
+                    'Code' => $data['Code'],
+                ]);
+                if(!empty($checkdata)){
+                    return $this->sendResponse('success', '驗證成功');
+                }
+                return $this->sendError('驗證碼錯誤');
+            }
+            return $this->sendError('驗證失敗');        
+        }
+        return $this->sendError('Method Not Allow', [], 405);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \app\core\Request
+     * @return \app\core\Response
+     */
+    public function resetPassword(Request $request)
+    {
+        if($request->isPost()){
+            $data = $request->getJson()?? ''; 
+            $checkdata = DbModel::findOne('reset_password', [
+                'Account' => $data['account'],
+                'Code' => $data['code'],
+            ]);
+            if(!empty($checkdata)){
+                if(strtotime($checkdata['Update_at']) > strtotime(time())){
+                    $requestModel = new ResetPassword();
+                    $requestModel->loadData($data);
+                    if($requestModel->validate()){
+                        $res = $this->memberService->updateUserPassword($requestModel->account,$requestModel->password);
+                        DbModel::delete('reset_password', [
+                            'Account' => $requestModel->account,
+                            'Code' => $requestModel->code
+                        ]);
+                        return $res? $this->sendResponse($res, "修改成功！") : $this->sendError("Error");
+                    }  
+                    return $this->sendError('兩次密碼輸入不一致');   
+                }
+                DbModel::delete('reset_password', [
+                    'Account' => $data['Account'],
+                    'Code' => $data['Code']
+                ]);
+                return $this->sendError('此驗證碼已過期');                 
+            }                
+        }
+        return $this->sendError('Method Not Allow', [], 405);
+    }
+
 }
-
-
-
-
-
