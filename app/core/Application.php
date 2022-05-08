525f7bfd30cda0a767c10c02817b0bbd4c147510
GXiang314 <akz232032@gmail.com>
2022-05-08 22:37:43 +0800

    add Controller attribute.

--- app/core/Application.php
+++ app/core/Application.php
@@ -9,6 +9,7 @@ class Application
 {
     public static Application $app;
     public static string $ROOT_DIR;
+    public Controller $controller;
     public Router $router;
     public Request $request;
     public Response $response;
@@ -18,6 +19,7 @@ class Application
     {
         self::$app = $this;
         self::$ROOT_DIR = dirname(__DIR__);
+        $this->controller = new Controller();
         $this->response = new Response();
         $this->request = new Request();
         $this->router = new Router($this->request, $this->response);
