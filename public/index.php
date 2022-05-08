47c60365574087e0c6d8b63a94abcc6b39cce41b
GXiang314 <akz232032@gmail.com>
2022-05-08 22:42:16 +0800

    add route.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\public;
 
+use app\controllers\api\LoginController;
 use app\core\Application;
 use app\controllers\api\MemberController;
 use app\controllers\api\UserController;
@@ -25,10 +26,17 @@ $config=[
 
 $app = new Application($config);
 
-$app->router->get('/api/manager/user', [MemberController::class,'index']);
+$app->router->get('/api/emailvalidate', [MemberController::class,'emailvalidate']); //信箱驗證
+$app->router->post('/api/login', [LoginController::class,'login']); //登入
+
 
 $app->router->get('/api/member', [MemberController::class,'show']);
+$app->router->put('/api/member/pwd', [MemberController::class,'updatePassword']);
+$app->router->put('/api/member/info', [MemberController::class,'updateIntroduction']);
 
+
+$app->router->get('/api/manager/user', [UserController::class,'index']);
 $app->router->post('/api/manager/useradd', [UserController::class,'useradd']);
+$app->router->delete('/api/manager/user', [UserController::class,'destroy']);
 
 $app->run();
