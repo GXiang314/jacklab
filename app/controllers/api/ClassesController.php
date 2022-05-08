a53d8177d814ea4fa9d66341e98ec6134670ab31
GXiang314 <akz232032@gmail.com>
2022-05-09 04:59:44 +0800

    add Classes Controller. Except authorization.

---
+++ app/controllers/api/ClassesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\controllers\api;
+
+use app\core\Controller;
+use app\core\Request;
+use app\requestModel\AddClassName;
+use app\requestModel\AddName;
+use app\requestModel\UpdateName;
+use app\services\ClassService;
+
+class ClassesController extends Controller{
+    
+    private $classService;
+
+    public function __construct()
+    {
+        $this->classService = new ClassService();
+    }
+
+    public function index()
+    {
+        $data = $this->classService->getAll();
+        return isset($data) ? $this->sendResponse($data, 'success') : $this->sendError('沒有資料');
+    }
+        
+    public function show(Request $request)
+    {
+        if($request->isGet()){
+            $id = $request->getBody()['Id'] ?? '';
+            $data = $this->classService->getStudent($id);
+            return isset($data) ? $this->sendResponse($data, 'success') : $this->sendError('沒有資料');
+        }
+        return $this->sendError('Method Not Allow.', [], 405);
+    }
+
+    public function store(Request $request)
+    {
+        if($request->isPost()){
+            $requestModel = new AddClassName();
+            $requestModel->loadData($request->getJson());
+            if($requestModel->validate()){
+                $result = $this->classService->add($requestModel->Name, $requestModel->Academic_Id);
+            }
+            return $result == 'success'? $this->sendResponse($result, 'success') : $this->sendError($result);
+        }
+        return $this->sendError('Method Not Allow.', [], 405);
+    }
+    
+    public function update(Request $request)
+    {
+        if($request->isPut()){
+            $requestModel = new UpdateName();
+            $requestModel->loadData($request->getJson());
+            if($requestModel->validate()){
+                $result = $this->classService->update($requestModel->Id,$requestModel->Name);
+            }
+            return $result == 'success'? $this->sendResponse($result, 'success') : $this->sendError($result);
+        }
+        return $this->sendError('Method Not Allow.', [], 405);
+    }
+    
+    public function destroy(Request $request)
+    {
+        if($request->isDelete()){
+            $id = $request->getBody()['Id'] ?? '';
+            $result = $this->classService->delete($id);
+            return $result == 'success'? $this->sendResponse($result, 'success') : $this->sendError($result);
+        }
+        return $this->sendError('Method Not Allow.', [], 405);
+    }
+}
