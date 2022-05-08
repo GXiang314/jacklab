94a67b35d9ccd89840b173fde26b03fa4602f25d
GXiang314 <akz232032@gmail.com>
2022-05-08 22:36:51 +0800

    login controller, add login action & response token.

--- app/controllers/api/LoginController.php
+++ app/controllers/api/LoginController.php
@@ -2,15 +2,48 @@
 namespace app\controllers\api;
 
 use app\core\Controller;
+use app\core\Request;
+use app\requestModel\Login;
+use app\services\JwtService;
+use app\services\MemberService;
 
 class LoginController extends Controller{
     private $memberService;
-    public function __construct()
-    {
-
+    private $jwtService;
+    public function __construct(){
+        $this->memberService = new MemberService();
+        $this->jwtService = new JwtService();
     }
-
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  \app\core\Request
+     * @return \app\core\Response
+     */
+    public function login(Request $request)
+    {        
+        if($request->isPost()){
+            $requestModel = new Login();
+            $data = $request->getJson();
+            $requestModel->loadData($data);
+            
+            if($requestModel->validate()){                
+                if($this->memberService->passwordCheck($data['Account'],$data['Password'])){
+                    if($this->memberService->isEmailValidate($data['Account'])){
+                        $member = $this->memberService->getAccountData($data['Account']);
+                        $token = $this->jwtService->Jwt_user_encode($member['Account'],$member['role']);
+                        return $this->sendResponse($token,"登入成功");
+                    }else{
+                        return $this->sendError('信箱未完成驗證，請查看信箱', [], 401);
+                    }
+                }
+                return $this->sendError('帳號或密碼輸入錯誤', [], 401);
+            }else{
+                return $this->sendError($requestModel->errors);
+            }
+        }
+        return $this->sendError("Method Not Allow.", [], 405);
+    }
 }
 
 
