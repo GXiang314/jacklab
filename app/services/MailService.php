ce69966acb2b84ca66a6c89e177fe850e70ec025
GXiang314 <akz232032@gmail.com>
2022-05-09 01:07:21 +0800

    send valiCode for reset password.

--- app/services/MailService.php
+++ app/services/MailService.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\services;
 
+use PDO;
+
 class MailService{
 
     public $headers;
@@ -12,10 +14,16 @@ class MailService{
     public function sendRegisterMail($name, $account,$password, $token)
     {
         $subject = "創建帳號通知";
-        $url = $_ENV['HOST']."/emailvalidate/?email=$account&token=$token";
-        $content = "使用者「{$name}」，您好：\r\n\r\n您的帳號是：{$account}\r\n您的密碼是：{$password}\r\n\r\n請點擊以下連結以完成驗證步驟：\r\n{$url}";
+        $url = $_ENV['HOST']."/emailvalidate?email=$account&token=$token";
+        $content = "使用者「{$name}」，你好：\r\n\r\n您的帳號是：{$account}\r\n您的密碼是：{$password}\r\n\r\n請點擊下方連結以完成驗證步驟\r\n{$url}";
         mail($account, $subject, $content, $this->header);
     }
 
-
+    public function sendForgetPasswordMail($name, $email, $code)
+    {
+        $subject = "{$code} 是你的 Jacklab 驗證碼";
+        $content = "使用者「{$name}」，你好：\r\n\r\n我們已收到你的重設密碼要求。\r\n請輸入下方驗證碼重設密碼(30分鐘內有效)：\r\n{$code}";
+        mail($email, $subject, $content, $this->header);
+        
+    }
 }
