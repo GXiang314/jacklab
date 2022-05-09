b0c43f9a9918ba9f97b2d67c03e6232d6adb915c
GXiang314 <akz232032@gmail.com>
2022-05-10 00:51:07 +0800

    update response.

--- app/core/Response.php
+++ app/core/Response.php
@@ -13,7 +13,9 @@ class Response
     public static function json(array $params, $status = 200)
     {
         header('Content-type: application/json');
-        self::setStatusCode($status);
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 dayself::setStatusCode($status);
 
         return json_encode($params);
     }
