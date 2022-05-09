ed50842b7a0278ec49a5fb744a039c9ecda5968c
GXiang314 <akz232032@gmail.com>
2022-05-10 00:23:16 +0800

    fix headers.

--- app/core/Cors.php
+++ app/core/Cors.php
@@ -6,11 +6,13 @@ class Cors
 {
     public function __construct()
     {
+        var_dump($_SERVER);
+
         // Allow from any origin
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
+        if (isset($_SERVER['Access-Control-Allow-Origin'])) {
             // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
             // you want to allow, and if so:
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Origin: {$_SERVER['Access-Control-Allow-Origin']}");
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
