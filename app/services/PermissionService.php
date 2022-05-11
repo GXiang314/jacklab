d3650c1ecfc11dfb956d5093644701251a050504
GXiang314 <akz232032@gmail.com>
2022-05-12 00:25:20 +0800

    permission updated

--- app/services/PermissionService.php
+++ app/services/PermissionService.php
@@ -22,23 +22,23 @@ class PermissionService{
     {
 
         $permission_Array = [        
-            '新增競賽記錄' =>'App\\Http\\Controllers\\api\\GameRecordController@store',
-            '編輯競賽記錄' =>'App\\Http\\Controllers\\api\\GameRecordController@update',
-            '刪除競賽記錄' =>'App\\Http\\Controllers\\api\\GameRecordController@destroy',
+            '新增競賽記錄' =>'GameRecordController@store',
+            '編輯競賽記錄' =>'GameRecordController@update',
+            '刪除競賽記錄' =>'GameRecordController@destroy',
         
-            '建立專案' =>'App\\Http\\Controllers\\api\\ProjectRecordController@store',
-            '編輯專案內容' =>'App\\Http\\Controllers\\api\\ProjectRecordController@update',
-            '刪除專案' =>'App\\Http\\Controllers\\api\\ProjectRecordController@destroy',
+            '建立專案' =>'ProjectRecordController@store',
+            '編輯專案內容' =>'ProjectRecordController@update',
+            '刪除專案' =>'ProjectRecordController@destroy',
             
-            '新增專案記錄' =>'App\\Http\\Controllers\\api\\ProjectRecordController@storeRecord',
-            '編輯專案記錄' =>'App\\Http\\Controllers\\api\\ProjectRecordController@updateRecord',
-            '刪除專案記錄' =>'App\\Http\\Controllers\\api\\ProjectRecordController@destroyRecord',
+            '新增專案記錄' =>'ProjectRecordController@storeRecord',
+            '編輯專案記錄' =>'ProjectRecordController@updateRecord',
+            '刪除專案記錄' =>'ProjectRecordController@destroyRecord',
 
-            '取得會議列表' =>'App\\Http\\Controllers\\api\\MeetController@index',
-            '取得該會議記錄' =>'App\\Http\\Controllers\\api\\MeetController@show',
-            '新增會議記錄' =>'App\\Http\\Controllers\\api\\MeetController@store',
-            '編輯會議記錄' =>'App\\Http\\Controllers\\api\\MeetController@update',
-            '刪除會議記錄' =>'App\\Http\\Controllers\\api\\MeetController@destroy',
+            '取得會議列表' =>'MeetController@index',
+            '取得該會議記錄' =>'MeetController@show',
+            '新增會議記錄' =>'MeetController@store',
+            '編輯會議記錄' =>'MeetController@update',
+            '刪除會議記錄' =>'MeetController@destroy',
         ];
         $dbCount = permission::count('permission');
         $arrCount = count($permission_Array);
