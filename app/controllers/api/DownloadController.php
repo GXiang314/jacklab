077d77f31846795ea657e92f71d2c2c1cf3b490d
GXiang314 <akz232032@gmail.com>
2022-05-11 08:59:54 +0800

    add game file download.

--- app/controllers/api/DownloadController.php
+++ app/controllers/api/DownloadController.php
@@ -4,22 +4,25 @@ namespace app\controllers\api;
 
 use app\core\Controller;
 use app\core\Request;
+use app\services\GameRecordService;
 use app\services\MeetService;
 
 class DownloadController extends Controller{
     
     private $meetService;
+    private $gameRecordService;
     
     public function __construct()
     {
         $this->meetService = new MeetService();
+        $this->gameRecordService = new GameRecordService();
     }
 
     public function download_Meet(Request $request)
     {
         if($request->isGet()){
             $id = $request->getBody()['id'] ?? '';
-            $file = $this->meetService->getFile($id);
+            $file = $this->meetService->getFile($id);       
             if(!empty($file)){
                 if (file_exists($file['Url'])) {
                     header('Content-Description: File Transfer');
@@ -36,6 +39,29 @@ class DownloadController extends Controller{
             return $this->sendError('找不到資源，請聯繫網站管理員');
         }        
     }
+
+    public function download_Game(Request $request)
+    {
+        if($request->isGet()){
+            $id = $request->getBody()['id'] ?? '';
+            $file = $this->gameRecordService->getFile($id);
+            if(!empty($file)){
+                if (file_exists($file['Url'])) {
+                    header('Content-Description: File Transfer');
+                    header('Content-Type: application/octet-stream');
+                    header('Content-Disposition: attachment; filename="'.basename($file['Name']).'"');
+                    header('Expires: 0');
+                    header('Cache-Control: must-revalidate');
+                    header('Pragma: public');
+                    header('Content-Length: ' . filesize($file['Url']));
+                    readfile($file['Url']);
+                    exit;
+                }
+            }            
+            return $this->sendError('找不到資源，請聯繫網站管理員');
+        }        
+    }
+
 /*
     public function download_Project($fileName)
     {
@@ -44,25 +70,6 @@ class DownloadController extends Controller{
             return Storage::download($file['Url'],$file['Name']);
         }
         return $this->sendError('找不到資源，請聯繫網站管理員');
-    }
-
-    public function download_Game($fileName)
-    {
-        $zip = $this->gameRecordService->game_FileZipGenerator($id);
-        try{
-            if($zip !='error'){
-                $path = storage_path('app\\game\\').$zip;
-                header('Content-disposition: attachment; filename='.$zip);
-                header('Content-type: application/zip');
-                readfile($path);
-            }
-        }catch(Exception){
-            return $this->sendError('找不到資源，請聯繫網站管理員');
-        }
-        finally{
-            Storage::delete('game\\'.$zip);
-        }
-        return ($zip != 'error')? $this->sendResponse($zip,'success') : $this->sendError('找不到資源，請聯繫網站管理員');
-    }
-    */
+    }*/    
+    
 }
