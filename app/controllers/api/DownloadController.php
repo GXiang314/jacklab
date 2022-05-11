fc943f71fcc8523077e5fe8197b99dc77256fe71
GXiang314 <akz232032@gmail.com>
2022-05-11 11:54:55 +0800

    add download projfile

--- app/controllers/api/DownloadController.php
+++ app/controllers/api/DownloadController.php
@@ -6,16 +6,19 @@ use app\core\Controller;
 use app\core\Request;
 use app\services\GameRecordService;
 use app\services\MeetService;
+use app\services\ProjectRecordService;
 
 class DownloadController extends Controller{
     
     private $meetService;
     private $gameRecordService;
+    private $projectRecordService;
     
     public function __construct()
     {
         $this->meetService = new MeetService();
         $this->gameRecordService = new GameRecordService();
+        $this->projectRecordService = new ProjectRecordService();
     }
 
     public function download_Meet(Request $request)
@@ -62,14 +65,27 @@ class DownloadController extends Controller{
         }        
     }
 
-/*
-    public function download_Project($fileName)
+
+    public function download_Project(Request $request)
     {
-        $file = $this->projectService->getFile($fileName);
-        if($file !=[]){
-            return Storage::download($file['Url'],$file['Name']);
-        }
-        return $this->sendError('找不到資源，請聯繫網站管理員');
-    }*/    
+        if($request->isGet()){
+            $id = $request->getBody()['id'] ?? '';
+            $file = $this->projectRecordService->getFile($id);
+            if(!empty($file)){
+                if (file_exists($file['Url'])) {
+                    header('Content-Description: File Transfer');
+                    header('Content-Type: application/octet-stream');
+                    header('Content-Disposition: attachment; filename="'.basename($file['Name']).'"');
+                    header('Expires: 0');
+                    header('Cache-Control: must-revalidate');
+                    header('Pragma: public');
+                    header('Content-Length: ' . filesize($file['Url']));
+                    readfile($file['Url']);
+                    exit;
+                }
+            }            
+            return $this->sendError('找不到資源，請聯繫網站管理員');
+        }        
+    }  
     
 }
