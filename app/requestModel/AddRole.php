ee62a8914fc66df7c12aff83b6cb8664f8f1cf45
GXiang314 <akz232032@gmail.com>
2022-05-12 14:40:08 +0800

    fix error "key"

--- app/requestModel/AddRole.php
+++ app/requestModel/AddRole.php
@@ -4,6 +4,7 @@
 namespace app\requestModel;
 
 use app\core\Model;
+use app\model\role;
 
 class AddRole extends Model{
 
@@ -17,7 +18,7 @@ class AddRole extends Model{
         return [            
             'Name' => [self::RULE_REQUIRED, 
             [self::RULE_MAX, 'max'=>20],
-            [self::RULE_UNIQUE, 'unique' => role::class]
+            [self::RULE_UNIQUE, 'class' => role::class]
         ],
             'Permission' => [self::RULE_REQUIRED],
         ];
