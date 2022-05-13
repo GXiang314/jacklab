36aaf12075d6ca81c3a85da2ac8d335579b82e44
GXiang314 <akz232032@gmail.com>
2022-05-13 19:24:57 +0800

    to mb_str...

--- app/core/Request.php
+++ app/core/Request.php
@@ -15,11 +15,11 @@ class Request
     public function getPath()
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
-        $position = strpos($path, '?');
+        $position = mb_strpos($path, '?');
         if ($position === false) {
             return $path;
         }
-        return substr($path, 0, $position + 1);
+        return mb_substr($path, 0, $position + 1);
     }
 
     public function method()
