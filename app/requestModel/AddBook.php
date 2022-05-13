a43324a39065eb3b923a4ab068c1d306748b7fc9
GXiang314 <akz232032@gmail.com>
2022-05-13 18:06:06 +0800

    add Request model.

---
+++ app/requestModel/AddBook.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+namespace app\requestModel;
+
+use app\core\Model;
+
+class AddBook extends Model{
+
+
+    public string $Title;
+    public string $Publisher;
+    public string $Time;
+    public string $ISBN;
+    public array $Authors;
+    public $File;
+
+    public function loadData($data)
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+        $this->File = $_FILES['File'] ?? null;
+    }
+
+    public function rules(): array
+    {
+        return [            
+            'Title' => [
+                self::RULE_REQUIRED, 
+                [self::RULE_MAX, 'max' => 50]
+            ],
+            'Publisher' => [
+                self::RULE_REQUIRED, 
+                [self::RULE_MAX, 'max' => 50]
+            ],
+            'Time' => [self::RULE_REQUIRED],
+            'ISBN' => [self::RULE_REQUIRED],
+            'Authors' => [self::RULE_REQUIRED],
+            'File' => [self::RULE_REQUIRED],
+        ];
+    }
+}
---
+++ app/requestModel/UpdateBook.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+namespace app\requestModel;
+
+use app\core\Model;
+
+class UpdateBook extends Model{
+
+    public int $Id;
+    public string $Title;
+    public string $Publisher;
+    public string $Time;
+    public string $ISBN;
+    public array $Authors;
+    public $File;
+
+    public function loadData($data)
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+        $this->File = $_FILES['File'] ?? null;
+    }
+
+    public function rules(): array
+    {
+        return [    
+            'Id' => [
+                self::RULE_REQUIRED
+            ],
+            'Title' => [
+                self::RULE_REQUIRED, 
+                [self::RULE_MAX, 'max' => 50]
+            ],
+            'Publisher' => [
+                self::RULE_REQUIRED, 
+                [self::RULE_MAX, 'max' => 50]
+            ],
+            'Time' => [self::RULE_REQUIRED],
+            'ISBN' => [self::RULE_REQUIRED],
+            'Authors' => [self::RULE_REQUIRED],
+        ];
+    }
+}
