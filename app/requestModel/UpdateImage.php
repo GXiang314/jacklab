e09bf7a959c1ea3d686beb8a7c0ed4269c3cfebd
GXiang314 <akz232032@gmail.com>
2022-05-19 23:45:21 +0800

    new update image request model

---
+++ app/requestModel/UpdateImage.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+namespace app\requestModel;
+
+use app\core\Model;
+
+class UpdateImage extends Model{
+
+
+    public int $Id;
+
+    public $Image;
+
+    public function loadData($data)
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+        $this->File = $_FILES['Image'] ?? null;
+    }
+
+    public function rules(): array
+    {
+        return [            
+            'Id' => [self::RULE_REQUIRED],
+            'Image' => [self::RULE_REQUIRED],
+        ];
+    }
+}
