e561d1bfe9791fc3b0895676900e8fe4deee0b8a
GXiang314 <akz232032@gmail.com>
2022-05-19 10:19:27 +0800

    response array (include token & admin?)

--- app/controllers/api/LoginController.php
+++ app/controllers/api/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\controllers\api;
 
 use app\core\Controller;
@@ -7,10 +8,12 @@ use app\requestModel\Login;
 use app\services\JwtService;
 use app\services\MemberService;
 
-class LoginController extends Controller{
+class LoginController extends Controller
+{
     private $memberService;
     private $jwtService;
-    public function __construct(){
+    public function __construct()
+    {
         $this->memberService = new MemberService();
         $this->jwtService = new JwtService();
     }
@@ -21,37 +24,29 @@ class LoginController extends Controller{
      * @return \app\core\Response
      */
     public function login(Request $request)
-    {        
-        if($request->isPost()){
+    {
+        if ($request->isPost()) {
             $requestModel = new Login();
             $data = $request->getJson();
             $requestModel->loadData($data);
-            
-            if($requestModel->validate()){                
-                if($this->memberService->passwordCheck($data['Account'],$data['Password'])){
-                    if($this->memberService->isEmailValidate($data['Account'])){
-                        $member = $this->memberService->getAccount($data['Account']);
-                        $token = $this->jwtService->Jwt_user_encode($member['Account'],$member['Role']);
 
-                        return $this->sendResponse($token,"登入成功");
-                    }else{
+            if ($requestModel->validate()) {
+                if ($this->memberService->passwordCheck($data['Account'], $data['Password'])) {
+                    if ($this->memberService->isEmailValidate($data['Account'])) {
+                        $member = $this->memberService->getAccount($data['Account']);
+                        $res['token'] = $this->jwtService->Jwt_user_encode($member['Account'], $member['Role']);
+                        $res['admin'] = $member['IsAdmin'];
+                        return $this->sendResponse($res, "登入成功");
+                    } else {
                         return $this->sendError('信箱未完成驗證，請查看信箱', [], 401);
                     }
-                }else{
-                    return $this->sendError('帳號或密碼輸入錯誤', [], 404);
+                } else {
+                    return $this->sendError('帳號或密碼輸入錯誤');
                 }
-            }else{
-                return $this->sendError($requestModel->errors);
+            } else {
+                return $this->sendError('欄位格式錯誤', $requestModel->errors);
             }
         }
         return $this->sendError("Method Not Allow.", [], 405);
     }
 }
-
-
-
-
-
-
-
-
