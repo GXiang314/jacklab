32ffa5a5666204cf3ded657a1108d8675b66bfcd
GXiang314 <akz232032@gmail.com>
2022-05-20 00:08:32 +0800

    fix attribute

--- app/requestModel/UpdateImage.php
+++ app/requestModel/UpdateImage.php
@@ -19,7 +19,7 @@ class UpdateImage extends Model{
                 $this->{$key} = $value;
             }
         }
-        $this->File = $_FILES['Image'] ?? null;
+        $this->Image = $_FILES['Image'] ?? null;
     }
 
     public function rules(): array
