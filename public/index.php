27bda008ff98c1035619a2438501d727e86f00e8
GXiang314 <akz232032@gmail.com>
2022-06-02 19:03:26 +0800

    add routing

--- public/index.php
+++ public/index.php
@@ -154,6 +154,7 @@ $app->router->put('/api/labinfo', [LabInfoController::class, 'update']); //修�
 $app->router->delete('/api/labinfo?', [LabInfoController::class, 'destroy']); //刪除研究室介紹
 
 $app->router->get('/api/album/select', [AlbumController::class, 'selector']); //取得所有相簿
+$app->router->get('/api/album', [AlbumController::class, 'index']); //取得所有相簿
 $app->router->get('/api/album/list', [AlbumController::class, 'index']); //取得所有相簿
 $app->router->get('/api/album/list?', [AlbumController::class, 'index']); //取得所有相簿(搜尋值)
 $app->router->get('/api/album?', [AlbumController::class, 'show']); //取得該相簿內容
